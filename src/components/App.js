c4e13be67eefa6ea644b1341ef57a70c09a17725
Luke Wickens <lukewickens1989@gmail.com>
2021-03-12 14:23:44 +0000

    initial file structure set up with some styling and separation of components.

--- src/components/App.js
+++ src/components/App.js
@@ -1,44 +1,54 @@
 import React, { Component } from 'react';
-import logo from '../logo.png';
-import './App.css';
+import MyPicture from '../assets/picture_me.JPG';
+import Navbar from './Navbar'
+import ReactTypingEffect from 'react-typing-effect';
+import './styles/App.css';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-          <a
-            className="navbar-brand col-sm-3 col-md-2 mr-0"
-            href="http://www.dappuniversity.com/bootcamp"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Dapp University
-          </a>
-        </nav>
+      <Navbar />
         <div className="container-fluid mt-5">
           <div className="row">
             <main role="main" className="col-lg-12 d-flex text-center">
               <div className="content mr-auto ml-auto">
-                <a
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img src={logo} className="App-logo" alt="logo" />
-                </a>
-                <h1>Dapp University Starter Kit</h1>
+              <img src={MyPicture} className='my_picture' alt='My Picture' />
+              <br />
+                <ReactTypingEffect
+              text={[
+                "Welcome to my portfolio",
+                "Bienvenue dans mon portfolio",
+                "benvenuto nel mio portfolio",
+                "Bienvenido a mi cartera",
+                "Willkommen in meinem Portfolio",
+                "私のポートフォリオへようこそ",
+                "欢迎来到我的投资组合",
+                "مرحبا بكم في محفظتي"
+              ]}
+              speed='40'
+              eraseSpeed='10'
+              eraseDelay='2000'
+              cursorRenderer={cursor => <h1>{cursor}</h1>}
+              displayTextRenderer={(text, i) => {
+                return (
+                  <h1>
+                    {text.split('').map((char, i) => {
+                      const key = `${i}`;
+                      return (
+                        <span
+                          key={key}
+                          style={i%2 === 0 ? {} : {}}
+                        >{char}</span>
+                      );
+                    })}
+                  </h1>
+                );
+              }}
+            />
                 <p>
-                  Edit <code>src/components/App.js</code> and save to reload.
+                  This website <strike>is running</strike> will be running on IPFS!
                 </p>
-                <a
-                  className="App-link"
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  LEARN BLOCKCHAIN <u><b>NOW! </b></u>
-                </a>
               </div>
             </main>
           </div>
---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './styles/Navbar.css';
+
+class Navbar extends Component {
+  render() {
+    return (
+        <nav className="navbar navbar-dark fixed-top flex-md-nowrap p-0">
+            <text className='name-nav'>Luke Wickens</text>
+        </nav>
+    );
+  }
+}
+
+export default Navbar;
